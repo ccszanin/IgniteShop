a0d66f530010ed9f5f5e44c5e464d9af795e659d
Carla Cristine <carlacs.cristine@gmail.com>
2024-02-21 20:19:51 -0300

    Configuring Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { styled } from  "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+} )
+
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <Button>Enviar</Button>
   );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,20 @@
+import { createStitches }  from '@stitches/react'
+
+
+export const {
+config,
+styled,
+css,
+globalCss,
+theme,
+keyframes,
+getCssText,
+createTheme
+
+}= createStitches({
+    theme:{
+      colors: {
+        rocketseat: '#8257e6',
+      }
+    }
+})
