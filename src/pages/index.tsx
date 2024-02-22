234e6deed9b9317a93f83e2a2f97dddfe09422a4
Carla Cristine <carlacs.cristine@gmail.com>
2024-02-22 16:54:27 -0300

    Creating carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,54 @@
-import { styled } from  "../styles"
+import { HomeContainer, Product } from "../styles/pages/home";
+import { useKeenSlider} from 'keen-slider/react'
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-} )
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+const [sliderRef] = useKeenSlider ({
+  slides:{
+    perView: 3,
+    spacing: 48,
+  }
+})
+
   return (
-    <Button>Enviar</Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product className="keen-slider__slide">
+            <Image src={camiseta1} alt="" width={520} height={480}/>
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+            <Image src={camiseta2} alt="" width={520} height={480}/>
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+            <Image src={camiseta3} alt="" width={520} height={480}/>
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+            <Image src={camiseta3} alt="" width={520} height={480}/>
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+        </Product>
+        
+    </HomeContainer>
   );
 }
