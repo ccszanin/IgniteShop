b80c96428657c436d871d7ac33de65e4470c7c01
Carla Cristine <carlacs.cristine@gmail.com>
2024-02-29 14:35:20 -0300

    SSG fallback

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,6 +2,7 @@ import { stripe } from '@/src/assets/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/src/styles/pages/products'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Stripe from 'stripe';
 
 interface ProductProps{
@@ -14,7 +15,11 @@ interface ProductProps{
   }
 }
 
-export default function Product ({ product }: ProductProps){
+export default function Product({ product }: ProductProps){
+ const { isFallback } = useRouter()
+ if (isFallback) {
+   return <p>Loading...</p>
+ }
   return(
    <ProductContainer>
      <ImageContainer>
@@ -38,7 +43,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [
       { params: { id: 'prod_PbwamBfBS6n4vC'}}
     ],
-    fallback: false,
+    fallback: true,
   }
 }
 
